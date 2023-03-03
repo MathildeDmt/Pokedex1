2626af7a9206fefbb3df56560da3c95dc040f954
MathildeDmt <mathilde.dumont@ig2i.centralelille.fr>
2023-03-03 12:09:31 +0100

    first commit

---
+++ src/app/pokemons/message.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor() { }
+
+  message?: String[];
+
+  addMessage(message:string){
+    this.message?.push(message);
+  }
+
+  clearMessage(){
+    this.message = [];
+  }
+}
